5f91a6227f6934d4528c7044804ed97c4fc40528
Marharyta Trofymenko <Marharyta_Trofymenko@epam.com>
2020-05-13 21:01:23 +0300

    chore: use Bearer

--- src/app/auth/authorization-token.interceptor.ts
+++ src/app/auth/authorization-token.interceptor.ts
@@ -1,27 +1,29 @@
 import {
-    HttpEvent,
-    HttpHandler,
-    HttpInterceptor,
-    HttpRequest
-    } from '@angular/common/http';
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './services/authentication.service';
 
 @Injectable()
 export class AuthorizationTokenInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const authToken = this.authenticationService.authTokenValue;
-        if (authToken && authToken.accessToken) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `${authToken.accessToken}` // TODO Bearer
-                }
-            });
-        }
-        return next.handle(request);
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const authToken = this.authenticationService.authTokenValue;
+    if (authToken && authToken.accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${authToken.accessToken}`,
+        },
+      });
     }
+    return next.handle(request);
+  }
 }
-
