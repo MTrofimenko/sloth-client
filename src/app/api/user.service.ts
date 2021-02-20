04f14cfeb1251d626443ede2c07526fa8de4ebac
Marharyta Trofymenko <marharyta.trofymenk@ihsmarkit.com>
2021-02-20 23:26:07 +0200

    feat: add prod api url, move to environments

--- src/app/api/user.service.ts
+++ src/app/api/user.service.ts
@@ -1,18 +1,18 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AppSettings } from '../appsettings';
 import { UserModel } from '../auth/auth.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   current() {
-    return this.http.get<UserModel>(`${AppSettings.apiUrl}/api/user/current`);
+    return this.http.get<UserModel>(`${environment.apiUrl}/api/user/current`);
   }
 
   getByName(namePart: string) {
-    return this.http.get<UserModel[]>(`${AppSettings.apiUrl}/api/user`, {
+    return this.http.get<UserModel[]>(`${environment.apiUrl}/api/user`, {
       params: new HttpParams({
         fromObject: { namePart },
       }),
